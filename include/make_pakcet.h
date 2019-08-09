576839e49bfbccfbae74baeb0de195839f337805
vhok74 <wogh8732@naver.com>
2019-08-09 16:54:46 +0900

    delete files

--- include/make_pakcet.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAKE_PAKCET_H
-#define MAKE_PAKCET_H
-
-void make_arp_request(char*,char*,int);
-void make_arp_request_multicast(char*, char*);
-void make_arp_reply(char*, char*,int);
-
-#endif // MAKE_PAKCET_H
--- include/my_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_INFO_H
-#define MY_INFO_H
-
-#endif // MY_INFO_H
-
-void my_mac(char*);
-void my_ip(char*);
-void make_ether_packet(int);
-void make_arp_packet(char*, char*,int);
